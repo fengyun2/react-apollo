84e13beddf559544e84ae066a45b588b6a56c5b2
ly2011 <1518550424@qq.com>
2018-02-08 22:43:15 +0800

    添加flow-bin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// 引入apollo-client
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import gql from "graphql-tag";
+
 import App from "./App";
 
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://q80vw8qjp.lp.gql.zone/graphql" }),
+  cache: new InMemoryCache()
+});
+client.query({ query: gql`{ hello }` }).then(console.log);
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+
+  document.getElementById("root")
+);
 registerServiceWorker();
