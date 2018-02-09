38304c769db846b1407fc6ba661b6fe696342c6e
ly2011 <1518550424@qq.com>
2018-02-09 08:49:55 +0800

    修改文件的格式

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
-const { makeExecutableSchema } = require("graphql-tools");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 // Some fake data
 const books = [
   {
     title: "Harry Potter and the Sorcerer's stone",
-    author: "J.K. Rowling"
+    author: 'J.K. Rowling',
   },
   {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
-  }
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
 ];
 
 // The GraphQL schema in string form
@@ -24,13 +24,13 @@ const typeDefs = `
 
 // The resolvers
 const resolvers = {
-  Query: { books: () => books }
+  Query: { books: () => books },
 };
 
 // Put together a schema
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 // Initialize the app
@@ -38,12 +38,12 @@ const app = express();
 app.use(cors());
 
 // The GraphQL endpoint
-app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 // GraphiQL, a visual editor for queries
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Start the server
 app.listen(4000, () => {
-  console.log("Go to http://localhost:4000/graphiql to run queries!");
+  console.log('Go to http://localhost:4000/graphiql to run queries!');
 });
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // 引入apollo-client
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import gql from "graphql-tag";
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 
-import App from "./App";
+import App from './App';
 
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
-  cache: new InMemoryCache()
+  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  cache: new InMemoryCache(),
 });
 client
   .query({
@@ -25,7 +25,7 @@ client
           author
         }
       }
-    `
+    `,
   })
   .then(console.log);
 ReactDOM.render(
@@ -33,6 +33,6 @@ ReactDOM.render(
     <App />
   </ApolloProvider>,
 
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 registerServiceWorker();
